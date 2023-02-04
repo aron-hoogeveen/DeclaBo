5110284e8b629524f44cee60422db0d254edd2c1
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2023-02-04 10:48:03 +0100

    Refactor ControllerAdvice
    The general exception handlers have been moved to a general class
    that handles all general errors. This removes coupling of classes
    and improves code reusability.

--- src/main/java/ch/bolkhuis/declabo/fund/FundErrorAdvice.java
+++ src/main/java/ch/bolkhuis/declabo/fund/FundErrorAdvice.java
@@ -1,16 +1,12 @@
 package ch.bolkhuis.declabo.fund;
 
 import ch.bolkhuis.declabo.exceptions.ConstraintViolationException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -21,39 +17,6 @@ public class FundErrorAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String fundNotFoundHandler(FundNotFoundException ex) {
         return ex.getMessage();
-    }
-
-    /**
-     * Handles the exception resulting from invalid method arguments.
-     *
-     * @param ex the exception
-     * @return a map with error descriptions
-     */
-    @ResponseBody
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
-
-    @ResponseBody
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ex.getMessage();
-    }
-
-    @ResponseBody
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleConstraintViolationException(ConstraintViolationException ex) {
-        return ex.getErrors();
     }
 
 }
--- src/main/java/ch/bolkhuis/declabo/user/UserErrorAdvice.java
+++ src/main/java/ch/bolkhuis/declabo/user/UserErrorAdvice.java
@@ -1,15 +1,10 @@
 package ch.bolkhuis.declabo.user;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class UserErrorAdvice {
@@ -19,25 +14,6 @@ public class UserErrorAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String userNotFoundHandler(UserNotFoundException ex) {
         return ex.getMessage();
-    }
-
-    /**
-     * Handles the exception resulting from invalid method arguments.
-     *
-     * @param ex the exception
-     * @return a map with error descriptions
-     */
-    @ResponseBody
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 
 }
